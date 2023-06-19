1650fe9e0f278c69dff21cde48bc2674d0cbf76d
AlissonSeraphim <projetoseraphim@gmail.com>
2023-06-19 17:01:53 -0300

    feat: Endpoint Get for talker id

--- src/index.js
+++ src/index.js
@@ -9,12 +9,25 @@ console.log(readTalkerData);
 
 // Const Status
 const OK = 200;
+const NOT_FOUND = 404;
 
 // Endpoints
-
 app.get('/talker', async (req, res) => {
   const talkers = await readTalkerData();
   return res.status(OK).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalkerData();
+
+  const idTalker = talkers.find((talker) => talker.id === Number(id));
+
+  if (!idTalker) {
+    return res.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(OK).json(idTalker);
 });
 
 // Para os Testes
